f196cc6ff96e045915c81052967023e15b73c9ca
Medisetti Eeshitha <meeshitha2@gmail.com>
2026-09-01 16:32:09 +0530

    Import React and useState in apiBase.js

--- frontend/src/services/apiBase.js
+++ frontend/src/services/apiBase.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+import { API_BASE_URL } from "../services/apiBase";
 const stripSlash = (url) => String(url || "").replace(/\/+$/, "");
 
 const envUrl = stripSlash(
