a0c0a6b2a75d5c1cd3c82b5f2878c9445d0c1da8
Medisetti Eeshitha <meeshitha2@gmail.com>
2026-09-01 18:28:21 +0530

    Update Wellness.jsx

--- frontend/src/pages/user/Wellness.jsx
+++ frontend/src/pages/user/Wellness.jsx
@@ -89,10 +89,7 @@ const Wellness = () => {
           ))}
         </section>
 
-        <div className="ah-journal-actions">
-          <Link to="/products" className="ah-journal-btn">
-            Explore the Shop
-          </Link>
+        
         </div>
       </main>
 
